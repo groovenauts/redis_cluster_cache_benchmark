1bed988b555b0f07cfb82298fe0010d94c41f3cb
akima <akm2000@gmail.com>
2014-04-21 11:19:12 +0900

    fix variable name

--- examples/w1r10.rb
+++ examples/w1r10.rb
@@ -5,6 +5,6 @@ i = rand(10000)
 key = i.to_s
 
 unless value = $client.get(key)
-  value = (1..i).map{|_| chars.sample}.join
+  value = (1..i).map{|_| $chars.sample}.join
   $client.set(key, value)
 end
